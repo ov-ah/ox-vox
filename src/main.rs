e4a742eb24bc74a10f93c67cb44958398a3ce328
ovah <admin@ovah.dev>
2026-05-13 03:58:15 -0700

    end of bevy quickstart

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 use bevy::prelude::*;
 
+pub struct HelloPlugin;
+
 #[derive(Component)]
 struct Person;
 
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("ovah dev".to_string())));
     commands.spawn((Person, Name("anaconda sniper".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
@@ -26,13 +33,17 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
     }
 }
 
-fn hello_world() {
-    println!("hello world!");
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, ((update_people, greet_people).chain()));
+    }
 }
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
         .run();
 }
